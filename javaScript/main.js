6c1e26cf0b98409a0374060bf0dd627151b2a55e
HungNguyen-99 <hungnvhe130726@gmail.com>
2023-06-11 18:33:56 +0700

    ky thuat throttle

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,11 +1,25 @@
 const initApp = () => {
     const button = document.querySelector('button');
-    button.addEventListener('click', debounce(clickOrder, 2000));
+    button.addEventListener('click', throttle(clickOrder, 5000));
 }
 
-const clickOrder = () => console.log('Order');
+const clickOrder = () => console.log('Order', new Date().getTime());
 
 document.addEventListener('DOMContentLoaded', initApp);
+
+const throttle = (fn, delay) => {
+    delay = delay || 0;
+    let last = 0;
+    return () => {
+        const now = new Date().getTime();
+        let a = now - last;
+        if (now - last < delay) {
+            return;
+        }
+        last = now;
+        fn();
+    }
+}
 
 const debounce = (fn, delay) => {
     delay = delay || 0;
