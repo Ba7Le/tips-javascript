904b641a4126a6acc467c22e95869be0623ec848
HungNguyen-99 <hungnvhe130726@gmail.com>
2023-06-17 14:45:01 +0700

    add 3

---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require('./src/app');
+
+const PORT = 3055;
+
+const server = app.listen(PORT, () => {
+    console.log('Server is running on PORT: ', PORT);
+})
+
+
+// ctrl c =>  
+process.on('SIGINT', () => {
+    server.close(() => console.log(`Exist server express`))
+})
---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const { default: helmet } = require('helmet');
+const compression = require('compression');
+
+//init middlewares
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
+
+//init db
+
+//init routes
+app.get('/', (req, res, next) => {
+    const strCompress = 'Hello HungDaiHiep';
+
+    return res.status(200).json({
+        message: 'wellcome to my chanel',
+        metadata: strCompress.repeat(10000)
+    })
+})
+
+//handle error
+
+module.exports = app;
