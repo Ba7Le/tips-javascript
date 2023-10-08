abcb5229bfc1ce4b2f30e922c2295a9545fea672
hungnvhe130725 <hungnvhe130726@gmail.com>
2023-10-08 20:25:08 +0700

    update s-4

--- src/app.js
+++ src/app.js
@@ -15,11 +15,8 @@ require('./dbs/init.mongodb');
 
 //init routes
 app.get('/', (req, res, next) => {
-    const strCompress = 'Hello HungDaiHiep';
-
     return res.status(200).json({
-        message: 'wellcome to my chanel',
-        metadata: strCompress.repeat(10000)
+        message: 'wellcome to my chanel'
     })
 })
 
